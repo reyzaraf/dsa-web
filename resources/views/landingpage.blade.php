e7f7af4b6dc04462549e5470fce19bf8ea743d69
Nevertheless <reyzaraf@gmail.com>
2023-06-13 17:35:33 +0700

    change footer to publi component

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -940,45 +940,7 @@
   </section>
 
 
-  <footer>
-    <div class="container" id="kontak">
-          <div class="footer_box">
-            <div class="footer_header">
-              <div class="footer_logo text-center">
-                <img width="350px" src="{{asset('dsa/assets/img/logo.png')}}" alt="evento">
-              </div>
-            </div>
-            <div class="footer_box_body text-center">
-              <p>
-                POLDA SULAWESI UTARA - DUTA SULUT AMAN.
-              </p>
-              <ul class="footer_social">
-                <li>
-                  <a href="#"><i class="ion-social-facebook"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="ion-social-twitter"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="ion-social-instagram"></i></a>
-                </li>
-              </ul>
-            </div>
-          </div>
-    </div>
-  </footer>
-  <div class="copyright_footer">
-    <div class="container">
-      <div class="text-center">
-        <p>
-          Copyright &copy;
-          <script>
-            document.write(new Date().getFullYear());
-          </script> Duta Sulut Aman
-        </p>
-      </div>
-    </div>
-  </div>
+  
     <!-- end content wrapper -->
 @endsection
 
